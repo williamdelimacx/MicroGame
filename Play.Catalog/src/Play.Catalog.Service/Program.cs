b5b548ecc0fab4bf26e99598ef6da3aa891bdd8d
William de Lima <williamdelimacx@gmail.com>
2025-12-02 10:40:56 -0300

    refac: common code

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
 
@@ -26,7 +27,11 @@ builder.Services.AddSingleton(serviceProvider =>
   return mongoClient.GetDatabase(serviceSettings.ServiceName);
 });
 
-builder.Services.AddSingleton<IItemsRepository, ItemsRepository>();
+builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
+{
+  var database = serviceProvider.GetService<IMongoDatabase>();
+  return new MongoRepository<Item>(database, "items");
+});
 
 builder.Services.AddControllers(options =>
 {
