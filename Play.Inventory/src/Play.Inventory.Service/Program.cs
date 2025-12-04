f173f995b13cdc30d66837d825c65eee8c9eb5c7
William de Lima <williamdelimacx@gmail.com>
2025-12-04 11:03:38 -0300

    feat: add the circuit breaker pattern

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -10,15 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddMongo()
-                .AddMongoRepository<InventoryItem>("inventoryitems");
-
-Random jitterer = new Random();
+    .AddMongoRepository<InventoryItem>("inventoryitems");
 
 builder.Services.AddHttpClient<CatalogClient>((sp, client) =>
 {
     client.BaseAddress = new Uri("https://localhost:5001");
 })
-// POLICY COM SUPORTE A DEPENDENCY INJECTION
+// Retry Policy com Exponential Backoff e Jitter
 .AddPolicyHandler((sp, request) =>
 {
     var logger = sp.GetRequiredService<ILogger<CatalogClient>>();
@@ -29,24 +27,42 @@ builder.Services.AddHttpClient<CatalogClient>((sp, client) =>
         .OrResult(r => !r.IsSuccessStatusCode)
         .WaitAndRetryAsync(
             5,
-            retryAttempt =>
-                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) +
-                TimeSpan.FromMilliseconds(jitter.Next(0, 1000)),
-            (outcome, timespan, retryAttempt, context) =>
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                + TimeSpan.FromMilliseconds(jitter.Next(0, 1000)),
+            onRetry: (outcome, timespan, retryAttempt, context) =>
             {
                 logger.LogWarning(
-                    "Delaying for {delay}s before retry {retryAttempt}",
-                    timespan.TotalSeconds,
-                    retryAttempt
+                    $"Delaying for {timespan.TotalSeconds}s before retry {retryAttempt}"
                 );
             }
         );
 })
+// Circuit Breaker Policy
+.AddPolicyHandler((sp, request) =>
+{
+    var logger = sp.GetRequiredService<ILogger<CatalogClient>>();
+
+    return Policy<HttpResponseMessage>
+        .Handle<HttpRequestException>()
+        .Or<TimeoutRejectedException>()
+        .OrResult(r => !r.IsSuccessStatusCode)
+        .CircuitBreakerAsync(
+            handledEventsAllowedBeforeBreaking: 3,
+            durationOfBreak: TimeSpan.FromSeconds(15),
+            onBreak: (outcome, timespan) =>
+            {
+                logger.LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+            },
+            onReset: () =>
+            {
+                logger.LogWarning("Closing the circuit...");
+            }
+        );
+})
+// Timeout Policy
 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
-
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
