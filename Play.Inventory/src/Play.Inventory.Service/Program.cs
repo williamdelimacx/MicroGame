64e94107f626787d461c86750e130530bfb52d62
William de Lima <williamdelimacx@gmail.com>
2025-12-04 10:41:17 -0300

    feat: add exponential backoff

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -2,19 +2,45 @@ using Play.Common.MongoDb;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
 
-builder.Services.AddHttpClient<CatalogClient>(client =>
+Random jitterer = new Random();
+
+builder.Services.AddHttpClient<CatalogClient>((sp, client) =>
 {
     client.BaseAddress = new Uri("https://localhost:5001");
+})
+// POLICY COM SUPORTE A DEPENDENCY INJECTION
+.AddPolicyHandler((sp, request) =>
+{
+    var logger = sp.GetRequiredService<ILogger<CatalogClient>>();
+    Random jitter = new Random();
+
+    return Policy<HttpResponseMessage>
+        .Handle<TimeoutRejectedException>()
+        .OrResult(r => !r.IsSuccessStatusCode)
+        .WaitAndRetryAsync(
+            5,
+            retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) +
+                TimeSpan.FromMilliseconds(jitter.Next(0, 1000)),
+            (outcome, timespan, retryAttempt, context) =>
+            {
+                logger.LogWarning(
+                    "Delaying for {delay}s before retry {retryAttempt}",
+                    timespan.TotalSeconds,
+                    retryAttempt
+                );
+            }
+        );
 })
 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
